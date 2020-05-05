0d54b2f3fc159b244e1ed7002e259474fccd8c7e
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 18:51:30 -0300

    Att

--- index.php
+++ index.php
@@ -5,48 +5,8 @@ require 'init.php';
 $app = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
 
 $app->get('/', function () {
-    $Home = new \App\Controllers\HomeController();
+    $Home = new \App\controllers\HomeController();
     $Home->index();
 });
-
-
-// adição de usuário
-// exibe o formulário de cadastro
-$app->get('/add', function () {
-    $Register = new \App\Controllers\RegisterController();
-    $Register->create();
-});
-
-// processa o formulário de cadastro
-$app->post('/add', function () {
-    $Register = new \App\Controllers\RegisterController();
-    $Register->store();
-});
-//
-//
-//// edição de usuário
-//// exibe o formulário de edição
-//$app->get('/edit/{id}', function ($request) {
-//    // pega o ID da URL
-//    $id = $request->getAttribute('id');
-//
-//    $UsersController = new \App\Controllers\UsersController;
-//    $UsersController->edit($id);
-//});
-//
-//// processa o formulário de edição
-//$app->post('/edit', function () {
-//    $UsersController = new \App\Controllers\UsersController;
-//    $UsersController->update();
-//});
-//
-//// remove um usuário
-//$app->get('/remove/{id}', function ($request) {
-//    // pega o ID da URL
-//    $id = $request->getAttribute('id');
-//
-//    $UsersController = new \App\Controllers\UsersController;
-//    $UsersController->remove($id);
-//});
 
 $app->run();
