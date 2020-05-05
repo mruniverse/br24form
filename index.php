8324469ce64464b76b2fc824179c44de03f61c69
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 20:16:57 -0300

    Att

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ require 'vendor/autoload.php';
 require 'init.php';
 
 CRest::installApp(true);
+$result = CRest::call('user.current');
+$name = array_column($result, 'NAME');
+echo $name[0];
+
 
 $app = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
 
