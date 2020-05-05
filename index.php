61a5fe9e3c305617d05e542a65052350f9813973
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 12:46:49 -0300

    Att

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $name = array_column($result, 'NAME');
 
 <div class="flex-center position-ref full-height">
     <div class="content">
-        <div class="guide-font">
+        <div class="title-guide">
             Bem-vindo <?=$name[0]?>!
         </div>
     </div>
