0b3e416f032e3d5388ebc48384fc6a73ac6a69a1
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 09:50:57 -0300

    Att

--- Views/layouts/header.php
+++ Views/layouts/header.php
@@ -3,60 +3,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>Titulo</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="../js/app.js" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script type="text/javascript"
             src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="../css/app.css" rel="stylesheet">
 </head>
 <body class="bcolor">
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="../">
-                BR 24 Form
-            </a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                        <li class="nav-item">
-                            <a class="nav-link" href=<?=$_SERVER['DOCUMENT_ROOT']."Views/auth/login.blade.php"?>>Login</a>
-                        </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="href=<?=$_SERVER['DOCUMENT_ROOT']."Views/auth/login.blade.php"?>">Register</a>
-                            </li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Usuario <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="">
-                                    Sair
-                                </a>
-                            </div>
-                        </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
