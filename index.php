b7e7ec6d508b2a75ab5cec6c2613216ad749b9d9
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 18:48:54 -0300

    Att

--- index.php
+++ index.php
@@ -22,31 +22,31 @@ $app->post('/add', function () {
     $Register = new \App\Controllers\RegisterController();
     $Register->store();
 });
-
-
-// edição de usuário
-// exibe o formulário de edição
-$app->get('/edit/{id}', function ($request) {
-    // pega o ID da URL
-    $id = $request->getAttribute('id');
-
-    $UsersController = new \App\Controllers\UsersController;
-    $UsersController->edit($id);
-});
-
-// processa o formulário de edição
-$app->post('/edit', function () {
-    $UsersController = new \App\Controllers\UsersController;
-    $UsersController->update();
-});
-
-// remove um usuário
-$app->get('/remove/{id}', function ($request) {
-    // pega o ID da URL
-    $id = $request->getAttribute('id');
-
-    $UsersController = new \App\Controllers\UsersController;
-    $UsersController->remove($id);
-});
+//
+//
+//// edição de usuário
+//// exibe o formulário de edição
+//$app->get('/edit/{id}', function ($request) {
+//    // pega o ID da URL
+//    $id = $request->getAttribute('id');
+//
+//    $UsersController = new \App\Controllers\UsersController;
+//    $UsersController->edit($id);
+//});
+//
+//// processa o formulário de edição
+//$app->post('/edit', function () {
+//    $UsersController = new \App\Controllers\UsersController;
+//    $UsersController->update();
+//});
+//
+//// remove um usuário
+//$app->get('/remove/{id}', function ($request) {
+//    // pega o ID da URL
+//    $id = $request->getAttribute('id');
+//
+//    $UsersController = new \App\Controllers\UsersController;
+//    $UsersController->remove($id);
+//});
 
 $app->run();
