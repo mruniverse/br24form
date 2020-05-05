53a3ab89d351663a79292a92b53520c524c291a7
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 11:54:11 -0300

    Att

--- Views/layouts/header.php
+++ Views/layouts/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
     <meta http-equiv="Content-Language" content="en-us">
+    <link rel="stylesheet" href="/Views/css/app.css"/>
     <link rel="stylesheet" href="/Views/css/style.css"/>
     <link rel="stylesheet" href="/Views/css/bitrix24-guide-style.css"/>
     <meta name="GENERATOR" content="Microsoft FrontPage 4.0">
@@ -11,8 +12,8 @@
 
     <body>
         <div class="bx-tabs-wrap">
-            <span class="bx-tab" href="/Views/home.blade.php">Home</span>
-            <span class="bx-tab bx-tab-active" href="/Views/auth/login.blade.php">Login</span>
+            <a href="/Views/auth/home.php"><span class="bx-tab">Home</span></a>
+            <a href="/Views/auth/login.php"><span class="bx-tab">Login</span></a>
             <a href="/Views/auth/register.php"><span class="bx-tab">Register</span></a>
             <div class="bx-tabs-line"></div>
         </div>
