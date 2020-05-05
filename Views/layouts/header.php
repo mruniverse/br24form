364e2eadcc3dacbed20e92324fcf2e32edd74985
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 12:23:01 -0300

    Att

--- Views/layouts/header.php
+++ Views/layouts/header.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="/Views/css/bitrix24-guide-style.css"/>
     <meta name="GENERATOR" content="Microsoft FrontPage 4.0">
     <meta name="ProgId" content="FrontPage.Editor.Document">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <title>Br24 Form</title>
 
 <body>
@@ -19,16 +21,22 @@
     </button>
 
     <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/Views/auth/home.php">Home <span class="sr-only">(página atual)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/Views/auth/login.php"><span class="sr-only">Login</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/Views/auth/register.php"><span class="sr-only">Register</span></a>
-            </li>
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+            <li><a data-toggle="tab" href="/Views/auth/register.php#menu1">Menu 1</a></li>
+            <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
+            <li><a data-toggle="tab" href="/Views/auth/register.php#menu3">Register</a></li>
         </ul>
+        <!--        <ul class="navbar-nav mr-auto">-->
+<!--            <li class="nav-item active">-->
+<!--                <a class="nav-link" href="/Views/auth/home.php">Home <span class="sr-only">(página atual)</span></a>-->
+<!--            </li>-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="/Views/auth/login.php"><span class="sr-only">Login</span></a>-->
+<!--            </li>-->
+<!--            <li class="nav-item">-->
+<!--                <a class="nav-link" href="/Views/auth/register.php"><span class="sr-only">Register</span></a>-->
+<!--            </li>-->
+<!--        </ul>-->
     </div>
 </nav>
