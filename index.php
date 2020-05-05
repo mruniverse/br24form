04135d14d2eb8a6ca07f7204163f0c4c371d912a
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 19:02:23 -0300

    Att

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-//require 'vendor/autoload.php';
-//require 'init.php';
+require 'vendor/autoload.php';
+require 'init.php';
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
