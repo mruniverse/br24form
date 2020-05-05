dce80643314f22c61aade2bdad9ce63765f4da0f
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 12:09:13 -0300

    Att

--- Views/layouts/header.php
+++ Views/layouts/header.php
@@ -10,10 +10,25 @@
     <meta name="ProgId" content="FrontPage.Editor.Document">
     <title>Br24 Form</title>
 
-    <body>
-        <div class="bx-tabs-wrap">
-            <a href="/Views/auth/home.php"><span class="bx-tab">Home</span></a>
-            <a href="/Views/auth/login.php"><span class="bx-tab">Login</span></a>
-            <a href="/Views/auth/register.php"><span class="bx-tab">Register</span></a>
-            <div class="bx-tabs-line"></div>
-        </div>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/Views/auth/home.php">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado"
+            aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/Views/auth/home.php">Home <span class="sr-only">(página atual)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/Views/auth/login.php"><span class="sr-only">Login</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/Views/auth/register.php"><span class="sr-only">Register</span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
