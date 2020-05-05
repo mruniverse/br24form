6f5d51fa10bad70cabc40f8b4464e9f0eb9672ce
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-05 11:01:13 -0300

    Att

--- index.php
+++ index.php
@@ -9,74 +9,22 @@ $name = array_column($result, 'NAME');
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    <meta http-equiv="Content-Language" content="en-us">
+    <link rel="stylesheet" href="Views/css/style.css"/>
+    <link rel="stylesheet" href="Views/css/bitrix24-guide-style.css"/>
+    <meta name="GENERATOR" content="Microsoft FrontPage 4.0">
+    <meta name="ProgId" content="FrontPage.Editor.Document">
 
     <title>Br24 Form</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
 </head>
 <body>
 <div class="flex-center position-ref full-height">
-    <div class="top-right links">
-        <a href="Views/home.blade.php">Home</a>
-        <a href="Views/auth/login.blade.php">Login</a>
-        <a href="Views/auth/register.php">Register</a>
+    <div class="bx-tabs-wrap">
+        <span class="bx-tab" href="Views/home.blade.php">Home</span>
+        <span class="bx-tab bx-tab-active" href="Views/auth/login.blade.php">Login</span>
+        <span class="bx-tab" href="Views/auth/register.php">Register</span>
+        <div class="bx-tabs-line"></div>
     </div>
 
     <div class="content">
