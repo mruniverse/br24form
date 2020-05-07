53b5fb81575e0cf2e7b3265f7e8352389be5b72f
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-07 04:39:32 -0300

    Att

--- app/crest/src/settings.php
+++ app/crest/src/settings.php
@@ -2,7 +2,7 @@
 //define('C_REST_CLIENT_ID','local.5eb2388b87d9c8.96183690');//Application ID
 //define('C_REST_CLIENT_SECRET','YO2g72B93qCmxmOzmn65JRTlhce9wVyfCRyu9bIdZCby6xgmYA');//Application key
 // or
-define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
+define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/ilshmnkwxpai7ns0/');//url on creat Webhook
 
 //define('C_REST_CURRENT_ENCODING','windows-1251');
 //define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
