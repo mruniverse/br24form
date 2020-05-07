bb878372ade8d50ab63f97baf0d7cdcd4f1f1de0
Yuri Lucas <yurilucaspg@gmail.com>
2020-05-07 02:44:31 -0300

    Att

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -13,7 +13,20 @@ class HomeController{
     * chama a view index.php do  /home   ou somente   /
     */
     public function index(){
-        print_r($_REQUEST['ONCRMDEALADD']);
+
+        print_r($_REQUEST);
+        writeToLog($_REQUEST, 'incoming');
+
+        /** * Write data to log file. * * @param mixed $data * @param string $title * * @return bool */
+        function writeToLog($data, $title = ''){
+            $log = "\n------------------------\n";
+            $log .= date("Y.m.d G:i:s") . "\n";
+            $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
+            $log .= print_r($data, 1);
+            $log .= "\n------------------------\n";
+            file_put_contents(getcwd() . '/hook.log', $log, FILE_APPEND);
+            return true;
+        }
 
         $user = new \Models\User("");
         $company = new \Models\Company("", "");
